b5d5d8cc8e6d06542bb3d5cf4d485d2084e9998d
LughWalle <civalfran1@gmail.com>
2022-11-20 23:01:30 -0300

    fix: home

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 
   
-  handleChange(e) {
+  const handleChange = (e) => {
     console.log("Fruit Selected!!");
     this.setState({ fruit: e.target.value });
   }
@@ -13,16 +13,6 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles.content}>
         <section className={styles.hero}>
-          {/* <InputText
-            type="text"
-            placeholder="Search"
-            name="typeText"
-          /> */}
-           <select value={this.state.fruit} onChange={this.handleChange} className={styles.select}>
-              {options.map((option) => (
-                <option value={option.value}>{option.label}</option>
-              ))}
-            </select>
         </section>
         <section className={styles.location}>
           <div className={styles.infoCity}>
